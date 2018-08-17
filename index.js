9553e3f7bb6af1f738c7240bb1cc39fc831672ef
lekenny <lekenny.cn@gmail.com>
2018-08-17 13:20:59 +0800

    1. 重命名问题

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {
     NativeModules
 }from 'react-native';
-const IFlyRecognizer = NativeModules.IFlyRecognizer;
+const IFlyRecognizerz = NativeModules.IFlyRecognizer;
 export default class IFlyRecognizer {
     /**
      * callbake 返回参数类型
@@ -10,10 +10,10 @@ export default class IFlyRecognizer {
      * { msg: '正确识别内容', status: 200 }
      */
     static startSpeech(callback) {
-        IFlyRecognizer.startSpeech(callback)
+        IFlyRecognizerz.startSpeech(callback)
     }
   
     static stopSpeech() {
-        IFlyRecognizer.stopSpeech()
+        IFlyRecognizerz.stopSpeech()
     }
   }
